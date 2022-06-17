194de791e3c8e52fc94d0e900c9a4a0757506728
Aqlice <camille.jumptower@gmail.com>
2022-06-17 15:29:03 +0200

    push p7

--- front/src/components/Posts.js
+++ front/src/components/Posts.js
@@ -8,6 +8,7 @@ function Posts({ key, fname, message, postUserId, postId, date }) {
     const token = JSON.parse(localStorage.token)
     const userId = JSON.parse(localStorage.userId)
     const [posts, setPosts] = useState([])
+    const [comments, setComments] = useState('')
 
     const deletePost = () => {
         axios({
@@ -28,11 +29,36 @@ function Posts({ key, fname, message, postUserId, postId, date }) {
             )
     }
 
+    const getComments = () => {
+        axios({
+            method: "GET",
+            url: `${process.env.REACT_APP_API_URL}api/posts/${postId} `,
+            /*headers: {
+                authorization: `Bearer ${token}`
+            },*/
+        })
+            .then((res) => {
+                setComments(res.data)
+                console.log(comments)
+                console.log(comments[0].comment)
+                if (res.data.error)
+                    console.log(res.data.error)
+            })
+            .catch((err) => {
+                console.log(err)
+            }
+            )
+    }
+
     return (
         <>
             <div className="post-container">
                 <p>{message}</p>
                 <p>{date}</p>
+                <li onClick={getComments} id="getComments">afficher les commentaires</li>
+                {comments ? (
+                    <p>{comments[0].comment}</p>
+                ) : ('')}
                 {postUserId === userId ? (
                     <li onClick={deletePost} id="delete-post" className="active-btn">supprimer</li>)
                     : ("")
